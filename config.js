1888b902f7189ff9c1a7af47f27f848e589abc00
davidtorner22 <davidgtorner@gmail.com>
2019-12-01 22:21:52 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,9 @@
+export default FireBaseKeys = {
+    apiKey: "AIzaSyAKqIOLcRPZFLNHjD9skK5NhyJLsD9HNMo",
+    authDomain: "socialapp-6680a.firebaseapp.com",
+    databaseURL: "https://socialapp-6680a.firebaseio.com",
+    projectId: "socialapp-6680a",
+    storageBucket: "socialapp-6680a.appspot.com",
+    messagingSenderId: "371375598417",
+    appId: "1:371375598417:web:e18ff2acc4f23806f10170"
+  };
